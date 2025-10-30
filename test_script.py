20b0efa1dcab91dea860980256cfdf4fe2b49c0a
Sevgi <sevgibasar564@gmail.com>
2025-10-30 22:04:18 +0300

    başarılı test denemesi

--- test_script.py
+++ test_script.py
@@ -2,8 +2,7 @@ def topla(a, b):
     return a + b
 
 def test_topla():
-    assert topla(2, 3) == 6
-    print('Test başarılı 🎯')
-
+    assert topla(2, 3) == 5
+    print("🎯 Test başarılı - log'da bu yazı görünecek!")
 
 test_topla()
